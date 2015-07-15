24f8c5a3ee6597ae2619859028bec94fccb17943
Nathan Pitman <hello@nathanpitman.com>
2015-07-15 13:56:16 +0100

    Create config.php

---
+++ system/third_party/np_freeform_forms/config.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+* NP Freeform Forms config file
+*
+* @package		np-freeform-forms
+* @author		Nathan Pitman <hello@nathanpitman.com>
+* @link			http://github.com/nathanpitman/np_freeform_forms
+* @copyright	Copyright 2015, Nathan Pitman
+*/
+
+if ( ! defined('NP_FFT_NAME'))
+{
+	define('NP_FFT_NAME',         'NP Freeform Forms');
+	define('NP_FFT_VERSION',      '1.1');
+	define('NP_FFT_DOCS',         'http://github.com/nathanpitman/np_freeform_forms');
+}
+
+$config['name']    = NP_FFT_NAME;
+$config['version'] = NP_FFT_VERSION;
+
+/* End of file ft.np_freeform_forms.php */
+/* Location: ./system/expressionengine/third_party/np_freeform_forms/ft.np_freeform_forms.php */
